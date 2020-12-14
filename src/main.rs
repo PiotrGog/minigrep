432e1f2ebe6b20623bd32ff21f7f9801d2c1d360
Piotr Gogola <gogola.piotr@interia.pl>
2020-12-14 23:12:04 +0100

    Add config constructor

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,27 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Given args: {:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Query: {}", query);
-    println!("Filename: {}", filename);
+    println!("Query: {}", config.query);
+    println!("Filename: {}", config.filename);
 
-    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let content =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("Given file content:\n{}", content);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        return Config { query, filename };
+    }
 }
